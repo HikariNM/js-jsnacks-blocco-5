e3c1e9624a76ead9f637a943fae0c3a0e774a3f8
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-21 22:52:14 +0100

    snack 04

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorName = posts.map(element => element.author);
+console.log(authorName);
