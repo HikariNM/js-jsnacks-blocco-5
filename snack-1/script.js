c94e63c6e8245d09753f00aa1e1f9352bb2f825b
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-21 22:34:13 +0100

    snack 01

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,10 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log(names)
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i])
+    
+}
